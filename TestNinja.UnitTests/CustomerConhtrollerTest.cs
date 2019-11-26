98d50055b5d9c147120b946c5e29f0e98eab103e
Oyewole Oni <ooni@eminenttechnology.com>
2019-11-26 11:50:55 -0600

    Unit test update

---
+++ TestNinja.UnitTests/CustomerConhtrollerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+   public class CustomerConhtrollerTest
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ShouldRetunNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            // NotFound
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            // NotFound or one of its derivatives
+            // Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+        [Test]
+        public void GetCustomer_IdIsNptZero_ShouldRetunOk()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
---
+++ TestNinja.UnitTests/ErrorLogerTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+   [TestFixture]
+    public class ErrorLogerTest
+    {
+        private ErrorLogger logger;
+
+        [SetUp]
+        public void Setup()
+        {
+            logger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_ShouldSetTHeLastErrorProperty()
+        {
+            // logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_Should_ThrowArgumentNullException(string error)
+        {
+           Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+    }
+}
