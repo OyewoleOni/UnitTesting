9d6f552aa4c018341b8c782c6c829d63bb02975e
Oyewole Oni <ooni@eminenttechnology.com>
2019-11-27 11:39:20 -0600

    Exercise 1 & 2 Completed

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator demeritPoints;
+
+        [SetUp]
+        public void SetUp()
+        {
+            demeritPoints = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_IfSpeedLessThan0OrGreaterThan300_ReturnArgumentOutOfRangeException(int speed)
+        {
+            Assert.That(() => demeritPoints.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(64,0)]
+        [TestCase(65,0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedResult)
+        {
+            var result = demeritPoints.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_IfValidSpeed_ReturnCalculatedPoint()
+        {
+            var result = demeritPoints.CalculateDemeritPoints(75);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/FizzBuzzTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+
+        [Test]
+        public void GetOutput_Should_Return_String_AndNotEmpty()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.TypeOf<string>());
+            Assert.That(result,Is.Not.Empty);
+        }
+
+        [Test]
+       
+        public void GetOutputIfDivisibleBy3And5_Should_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutputIfDivisibleByThree_Should_Return_Fizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void GetOutputIfDivisibleByFive_Should_Return_Fizz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutputIfNotDivisibleByThreeOrFive_Should_Return_TheSameNumber()
+        {
+            var result = FizzBuzz.GetOutput(1);
+
+            Assert.That(result, Is.EqualTo("1"));
+        }
+    }
+}
