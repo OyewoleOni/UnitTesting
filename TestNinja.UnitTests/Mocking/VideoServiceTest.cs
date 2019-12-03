5c0f95908498db31aad2b5d39538305ecfea750e
Oyewole Oni <ooni@eminenttechnology.com>
2019-12-03 12:06:31 -0600

    Dependency Injection Explained

--- TestNinja.UnitTests/Mocking/VideoServiceTest.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTest.cs
@@ -14,9 +14,10 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService();
+            var service = new VideoService(new FakeFileReader());
+            //service.FileReader = new FakeFileReader();
 
-            var result = service.ReadVideoTitle(new FakeFileReader());
+            var result = service.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
